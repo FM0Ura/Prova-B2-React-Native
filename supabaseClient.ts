029b8a03bf1dd42c226ae7916ae5e1e003d386df
Felipe Angelo Zucolotto Moura <fanzm.013@gmail.com>
2024-11-18 10:57:58 -0300

    Add files via upload

---
+++ supabaseClient.ts
@@ -0,0 +1,7 @@
+// supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = '';
+const supabaseKey = '';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
