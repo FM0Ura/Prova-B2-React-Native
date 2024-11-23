ff69da13797cd652e5acf5efcf41562601ee90c5
Felipe Angelo Zucolotto Moura <fanzm.013@gmail.com>
2024-11-22 21:25:27 -0300

    Delete supabaseClient.ts

--- supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-// supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = '';
-const supabaseKey = '';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
